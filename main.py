6d2ef8e06552e2703d83127fbf420b4e2dbda7c6
abc <abc@gmail.com>
2022-01-16 13:32:22 -0500

    modified all the strings to numeric characters

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-bitcoin_price = 43000
-eth_price = 4200
-ada_price = 1.80
+bitcoin_price = int(43000)
+eth_price = int(4200)
+ada_price = int(1.80)
 
 def crypto_pre(year, annual_correction):
+    correction = int(annual_correction)
     if year > 2021:
-     print(f"Bitcoin predicted price for {year} is {bitcoin_price * annual_correction}")
-     print(f"Ethereum predicted price for {year} is {eth_price* annual_correction}")
-     print(f"Cardano predicted price for {year} is {ada_price * annual_correction}")
+     print(f"Bitcoin predicted price for {year} is {bitcoin_price * correction}")
+     print(f"Ethereum predicted price for {year} is {eth_price* correction}")
+     print(f"Cardano predicted price for {year} is {ada_price * correction}")
     else:
      print("Please enter valid year i.e. any year above 2021")
 
