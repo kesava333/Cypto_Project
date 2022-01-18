52df6dbf68d453525b28d74538de1a67cb831a6f
abc <abc@gmail.com>
2022-01-18 15:45:30 -0500

    Added while loop and accepting user input for price correction

--- main.py
+++ main.py
@@ -1,24 +1,29 @@
 bitcoin_price = int(43000)
 eth_price = int(4200)
-ada_price = 1.8
+ada_price = float(1.8)
 
-def crypto_pre(year, correction):
+def crypto_pre(year_no_element, correction):
   try:
-    year_input = int(year)
+    year_input = int(year_no_element)
     if year_input > 2021 & year_input >0:
-     print(f"Bitcoin predicted price for {year_input} is {bitcoin_price * correction}")
-     print(f"Ethereum predicted price for {year_input} is {eth_price* correction}")
-     print(f"Cardano predicted price for {year_input} is {ada_price * correction}")
-    else:
-     print("Please enter valid year i.e. any year above 2021")
+       print(f"Bitcoin predicted price for {year_input} is {bitcoin_price * correction}")
+       print(f"Ethereum predicted price for {year_input} is {eth_price* correction}")
+       print(f"Cardano predicted price for {year_input} is {ada_price * correction}")
+    elif year_input <=0:
+       print("Please enter valid year i.e. any year above 2021 or a non negative number or non zero")
   except ValueError:
-      print("Please enter a Number, don't destroy my APP")
+       print("Please enter a Number, don't destroy my APP")
 
 
 ## I have to work with user input
-year_no = input("Hey! Please enter the Year, we will send you the predicted price \n input ---->  ")
-print(f"Price Prediction For Year {year_no}")
-crypto_pre(year_no, 1.2)
+year_no=0
+while year_no != "exit":
+   year_no = input("Hey! Please enter the Year, we will send you the predicted price \n input ---->  ")
+   correction_input = float(input(f"Please enter Correction Input:\n"))
+   ##print(f"Price Prediction For Year {year_no}")
+   for year_no_element in year_no.split(","):
+       print(f"Price Prediction For Year {year_no_element}")
+       crypto_pre(year_no_element, correction_input)
 
 
 
