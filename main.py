f8e817e981b05fb7dac9a8031ca5da9aa3d7e7c3
abc <abc@gmail.com>
2022-01-18 11:25:40 -0500

    Integrated with TRY and CATCH functions

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
 bitcoin_price = int(43000)
 eth_price = int(4200)
-ada_price = int(1.80)
+ada_price = 1.8
 
-def crypto_pre(year, annual_correction):
-    correction = int(annual_correction)
-    if year > 2021:
-     print(f"Bitcoin predicted price for {year} is {bitcoin_price * correction}")
-     print(f"Ethereum predicted price for {year} is {eth_price* correction}")
-     print(f"Cardano predicted price for {year} is {ada_price * correction}")
+def crypto_pre(year, correction):
+  try:
+    year_input = int(year)
+    if year_input > 2021 & year_input >0:
+     print(f"Bitcoin predicted price for {year_input} is {bitcoin_price * correction}")
+     print(f"Ethereum predicted price for {year_input} is {eth_price* correction}")
+     print(f"Cardano predicted price for {year_input} is {ada_price * correction}")
     else:
      print("Please enter valid year i.e. any year above 2021")
+  except ValueError:
+      print("Please enter a Number, don't destroy my APP")
 
 
-print("  \003 Price Prediction For Year 2022   \003")
-crypto_pre(2022, 1.2)
-print("\n \003  \b Price Prediction for Year 2023  \003")
-crypto_pre(2023, 1.4)
-crypto_pre(2021,1.0)
+## I have to work with user input
+year_no = input("Hey! Please enter the Year, we will send you the predicted price \n input ---->  ")
+print(f"Price Prediction For Year {year_no}")
+crypto_pre(year_no, 1.2)
+
+
+
 
 
